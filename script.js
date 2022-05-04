6e0dce714e0ff32ce14937bc889cdcf6224716bf
Don Forrest <forrest@philippinedev.com>
2022-05-04 22:24:45 +0800

    Get user github repositories

--- script.js
+++ script.js
@@ -9,10 +9,20 @@ async function getUser(username) {
     const { data } = await axios(APIURL + username)
 
     createUserCard(data)
+    getRepos(username)
   } catch(err) {
     if (err.response.status == 404) {
       createErrorCard("No profile with this username")
     }
+  }
+}
+
+async function getRepos(username) {
+  try {
+    const { data } = await axios(APIURL + username + "/repos?sort=created")
+
+  } catch(err) {
+    createErrorCard("Problem fetching repos")
   }
 }
 
