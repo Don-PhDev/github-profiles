e629f1640545b603837850798468a1603267b13d
Don Forrest <forrest@philippinedev.com>
2022-05-04 22:23:04 +0800

    Create card for info

--- script.js
+++ script.js
@@ -7,9 +7,44 @@ const search = document.getElementById("search")
 async function getUser(username) {
   try {
     const { data } = await axios(APIURL + username)
+
+    createUserCard(data)
   } catch(err) {
     if (err.response.status == 404) {
-      console.log("No profile with this username")
+      createErrorCard("No profile with this username")
     }
   }
+}
+
+function createUserCard(user) {
+  const userID = user.name || user.login
+  const userBio = user.bio ? `<p>${user.bio}</p>` : ""
+  const cardHTML = `
+  <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${userID}</h2>
+      ${userBio}
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+      <div id="repos"></div>
+    </div>
+  </div>
+  `
+  main.innerHTML = cardHTML
+}
+
+function createErrorCard(msg) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${msg}</h1>
+    </div>
+  `
+
+  main.innerHTML = cardHTML
 }
